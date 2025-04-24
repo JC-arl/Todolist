a471ca6357b80e1d28b14c780f2d6773813110c7
JC-alr <hwang1535@naver.com>
2025-04-24 11:40:43 +0900

    mainpage

--- todo/url.py
+++ todo/url.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login_page, name='login_page')
+    path('', views.login_main, name='login_main'),
+    path('login_signin/', views.login_signin, name='login_signin'),
+    path('login_signup/', views.login_signup, name='login_signup'),
+    path('main_page/', views.main_page, name='main_page'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
-def login_page(request):
+def login_main(request):
     return render(request, 'login.html',{})
+
+def login_signin(request):
+    return render(request, 'login_signin.html',{})
+
+def login_signup(request):
+    return render(request, 'login_signup.html',{})
+def main_page(request):
+    return render(request, 'main_page.html',{})
